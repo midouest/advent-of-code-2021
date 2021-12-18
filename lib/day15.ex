aa880f2d512490a0d10f235bb1746d544251f6d2
Robin <midouesterner@gmail.com>
2021-12-18 15:45:17 -0600

    merge grid into day 15

--- lib/day15.ex
+++ lib/day15.ex
@@ -1,5 +1,5 @@
 defmodule Advent.Day15 do
-  alias Advent.Grid
+  alias Advent.Day15.Grid
   import Astar
 
   def load_puzzle(), do: Advent.read("data/day15.txt")
@@ -18,7 +18,7 @@ defmodule Advent.Day15 do
     grid =
       lines
       |> Grid.parse(&String.to_integer/1)
-      |> repeat(tiles)
+      |> Grid.repeat(tiles)
 
     {width, height} = Grid.size(grid)
 
@@ -29,6 +29,34 @@ defmodule Advent.Day15 do
     astar({nbs, dist, h}, {0, 0}, {width - 1, height - 1})
     |> Enum.map(&Grid.get(grid, &1))
     |> Enum.sum()
+  end
+end
+
+defmodule Advent.Day15.Grid do
+  defstruct cells: Map.new(), width: 0, height: 0
+
+  alias __MODULE__, as: Grid
+
+  def parse(lines, fun \\ &Function.identity/1) do
+    height = length(lines)
+    width = String.length(List.first(lines))
+
+    cells =
+      lines
+      |> Stream.map(fn line ->
+        line
+        |> String.graphemes()
+        |> Stream.map(fun)
+        |> Enum.with_index()
+      end)
+      |> Stream.with_index()
+      |> Enum.reduce(%{}, fn {cols, y}, cells ->
+        Enum.reduce(cols, cells, fn {value, x}, cells ->
+          Map.put(cells, {x, y}, value)
+        end)
+      end)
+
+    %Grid{cells: cells, width: width, height: height}
   end
 
   def repeat(%Grid{cells: cells, width: width, height: height}, tiles) do
@@ -47,5 +75,39 @@ defmodule Advent.Day15 do
       end
 
     %Grid{cells: cells, width: width * tiles, height: height * tiles}
+  end
+
+  def size(%Grid{width: width, height: height}), do: {width, height}
+
+  def get(%Grid{cells: cells}, coord), do: Map.fetch!(cells, coord)
+
+  def neighbors(%Grid{width: width, height: height}, {x, y} = coord) do
+    coords = if y > 0, do: [up(coord)], else: []
+    coords = if y < height - 1, do: [down(coord) | coords], else: coords
+    coords = if x > 0, do: [left(coord) | coords], else: coords
+    if x < width - 1, do: [right(coord) | coords], else: coords
+  end
+
+  defp up({x, y}), do: {x, y - 1}
+  defp down({x, y}), do: {x, y + 1}
+  defp left({x, y}), do: {x - 1, y}
+  defp right({x, y}), do: {x + 1, y}
+
+  def print(
+        %Grid{cells: cells, width: width, height: height},
+        fun \\ &Function.identity/1,
+        device \\ :stdio
+      ) do
+    IO.write(device, "\n")
+
+    Enum.each(0..(height - 1), fn y ->
+      Enum.each(0..(width - 1), fn x ->
+        IO.write(device, fun.(Map.fetch!(cells, {x, y})))
+      end)
+
+      IO.write(device, "\n")
+    end)
+
+    IO.write(device, "\n")
   end
 end
